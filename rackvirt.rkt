c46ac3eefb69ea09ce8c96496a54fafc7f13d7c8
Hunter Jones <hjones2199@gmail.com>
2020-06-25 17:18:13 -0500

    Added domain creation binding

--- rackvirt.rkt
+++ rackvirt.rkt
@@ -9,28 +9,44 @@
            virConnectGetType
            virConnectGetURI
            virConnectIsSecure
+           virDomainDefineXML
+           virDomainCreate
            with-open-vir)
   
-  (define-ffi-definer define-libvirt
-    (ffi-lib "libvirt_driver_interface"))
+  (define-ffi-definer define-virtiface
+    (ffi-lib "/usr/lib/libvirt/connection-driver/libvirt_driver_interface"))
   
-  (define-libvirt virConnectOpen (_fun _string -> _pointer))
-  (define-libvirt virConnectClose (_fun _pointer -> _int))
-  (define-libvirt virConnectGetHostname (_fun _pointer -> _string))
-  (define-libvirt virConnectGetCapabilities (_fun _pointer -> _string))
-  (define-libvirt virConnectGetType (_fun _pointer -> _string))
-  (define-libvirt virConnectGetURI (_fun _pointer -> _string))
-  (define-libvirt virConnectIsSecure (_fun _pointer -> _int))
+  (define-virtiface virConnectOpen (_fun _string -> _pointer))
+  (define-virtiface virConnectClose (_fun _pointer -> _int))
+  (define-virtiface virConnectGetHostname (_fun _pointer -> _string))
+  (define-virtiface virConnectGetCapabilities (_fun _pointer -> _string))
+  (define-virtiface virConnectGetType (_fun _pointer -> _string))
+  (define-virtiface virConnectGetURI (_fun _pointer -> _string))
+  (define-virtiface virConnectIsSecure (_fun _pointer -> _int))
+  (define-virtiface virDomainDefineXML (_fun _pointer _string -> _pointer))
+  (define-virtiface virDomainCreate (_fun _pointer -> _int))
 
   (define-syntax-rule (with-open-vir (var uri) exprs ...)
-    (let ([var (virConnectOpen uri)])
-      exprs ...
-      (virConnectClose uri))))
+    (let* ([var (virConnectOpen uri)] [ans (begin exprs ...)])
+      (virConnectClose var)
+      ans)))
 
-;; (module+ test
-;;   (require (submod ".." capi))
-;;   (let ([default-host (virConnectOpen "")])
-;;     (check-equal? (virConnectGetHostname default-host) (system "hostname -f"))))
+(define domain-xexpr
+  `(domain
+    ((type "kvm"))
+    (name ,(symbol->string (gensym)))
+    (memory
+     ((unit "GiB"))
+     "2")
+    (vcpu
+     ((placement "static"))
+     "1")
+    (clock ((offset "utc")))
+    (os
+     (type
+      ((arch "x86_64"))
+      "hvm")
+     (boot ((dev "hd"))))))
 
 (module+ main
   (require (submod ".." capi))
@@ -40,8 +56,11 @@
   
   (with-open-vir (x "")
     (printf "Hostname: ~a~%" (virConnectGetHostname x))
-    (printf "Capabilities: ~%~a~%" (serialize-capabilities x))
+    ;; (printf "Capabilities: ~%~a~%" (serialize-capabilities x))
     (printf "Connection Type: ~a~%" (virConnectGetType x))
     (printf "Connection URI: ~a~%" (virConnectGetURI x))
     (printf "Connection ~a secure~%"
-            (if (equal? (virConnectIsSecure x) 1) "is" "is not"))))
+            (if (equal? (virConnectIsSecure x) 1) "is" "is not"))
+    (printf "~a~%" (xexpr->string domain-xexpr))
+    (let ([dom1 (virDomainDefineXML x (xexpr->string domain-xexpr))])
+      (virDomainCreate dom1))))
